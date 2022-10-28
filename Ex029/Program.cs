fa6c541caf16c9ba068c6d937b3f27ae90df2728
Danil <danil-8951@mail.ru>
2022-10-28 20:48:43 +0500

    Доработали

--- Ex029/Program.cs
+++ Ex029/Program.cs
@@ -4,6 +4,7 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
+/*
 int [] array = new int [8];
 Random rand = new Random();
 for (int i = 0; i < array.Length; i++)
@@ -12,4 +13,18 @@ for (int i = 0; i < array.Length; i++)
 }
 
 Console.WriteLine(string.Join(",", array ));
+*/
 
+// получить все 8 чисел за одно нажатие Enter от пользователя.подсказка: 
+// использовать метод Split();*/
+
+
+// Console.WriteLine("Введите элементы массива, разделяя их одним пробелом");
+
+Console.WriteLine("Введите число : ");
+string NumbersStr = Console.ReadLine();
+string[] array = NumbersStr.Split();
+Console.Write("[");
+for (int i = 0; i < array.Length-1; i++) 
+Console.Write(array[i]+", ");
+Console.WriteLine(array[array.Length-1]+"]");
